a89a7ae7b16a6cc14eb2a4d6fd9e38e2de02a798
Miguel Vargas <puntadelanza86@gmail.com>
2020-08-02 10:39:47 -0500

    Se envia el objeto creado, con los ids correspondientes, en base a la version 1 del contrato del API formularios

--- src/services/formsService.js
+++ src/services/formsService.js
@@ -44,71 +44,9 @@ exports.create = async (req, res) => {
     res.status(200).json({
       status: 'success',
       serverTime: Date.now(),
-      data: [
-        {
-          id: 1,
-          name: 'Actividad 1',
-          date: new Date().setDate(new Date().getMinutes() - 30),
-          client: {
-            id: 2,
-            name: 'Pepsi',
-          },
-          operator: {
-            id: 12,
-            name: 'Patron',
-          },
-          zone: {
-            id: 3,
-            name: 'Sur Oriente',
-          },
-          state: {
-            id: 1,
-            name: 'Sin asignar',
-          },
-        },
-        {
-          id: 2,
-          name: 'Actividad 2',
-          date: new Date().setDate(new Date().getMinutes() - 60),
-          client: {
-            id: 1,
-            name: 'Cumis',
-          },
-          operator: {
-            id: 11,
-            name: 'Jerson',
-          },
-          zone: {
-            id: 5,
-            name: 'Sur',
-          },
-          state: {
-            id: 4,
-            name: 'Sin firma',
-          },
-        },
-        {
-          id: 3,
-          name: 'Actividad 3',
-          date: new Date().setDate(new Date().getMinutes() - 120),
-          client: {
-            id: 3,
-            name: 'Vanti',
-          },
-          operator: {
-            id: 56,
-            name: 'Juan',
-          },
-          zone: {
-            id: 6,
-            name: 'Norte',
-          },
-          state: {
-            id: 5,
-            name: 'Cerrada',
-          },
-        },
-      ],
+      data: {
+        test,
+      },
     });
   } catch (err) {
     res.status(500).json({
