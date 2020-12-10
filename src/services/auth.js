2e86892ec4fd50254df4d19c3a34415a2210ec32
Miguel Angel Vargas <mavargas@HEXACTA.COM>
2020-12-09 20:29:14 -0500

    Se corrige comportamiento de tokens

--- src/services/auth.js
+++ src/services/auth.js
@@ -21,46 +21,56 @@ exports.validRoles = (...roles) => {
 };
 
 exports.validateRefreshToken = async (accessTkn) => {
-  const tokenData = await promisify(jwt.verify)(
-    accessTkn,
-    process.env.JWT_SECRET
-  );
-  if (!tokenData.email) {
-    throw new AppError('Invalids token', 401);
+  try{
+    const tokenData = await promisify(jwt.verify)(
+      accessTkn,
+      process.env.JWT_SECRET
+    );
+    if (!tokenData.email) {
+      throw new AppError('Invalids token', 401);
+    }
+    const user = await userModel.getUser(tokenData.email);
+    if (user.rows <= 0) {
+      throw new AppError('Invalids token', 401);
+    }
+    if (
+      user.rows[0].changedPasswordAt &&
+      tokenData.iat < user.rows[0].changedPasswordAt.getTime() / 1000
+    ) {
+      throw new AppError('Undefined token error', 500);
+    }
+    return user;
+  }catch(e){
+    if(e.name === 'TokenExpiredError') throw new AppError('Invalids token', 408);
+    throw new AppError('Invalids token', 408);  
   }
-  const user = await userModel.getUser(tokenData.email);
-  if (user.rows <= 0) {
-    throw new AppError('Invalids token', 401);
-  }
-  if (
-    user.rows[0].changedPasswordAt &&
-    tokenData.iat < user.rows[0].changedPasswordAt.getTime() / 1000
-  ) {
-    throw new AppError('Invalids token', 408);
-  }
-  return user;
 };
 
 exports.getVerifyMiddleware = wrapAsyncFn(async (req, res, next) => {
-  const autorization = req.get('Authorization');
-  if (!autorization) {
-    return next(new AppError('Token missing', 401));
+  try{
+    const autorization = req.get('Authorization');
+    if (!autorization) {
+      return next(new AppError('Token missing', 401));
+    }
+    const token = autorization.split(' ')[1];
+    const tokenData = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    if (!tokenData.email) {
+      return next(new AppError('Invalids token', 401));
+    }
+    const user = await userModel.getUser(tokenData.email);
+    if (user.rows <= 0) {
+      return next(new AppError('User does not exist', 401));
+    }
+    if (
+      user.rows[0].changedPasswordAt &&
+      tokenData.iat < user.rows[0].changedPasswordAt.getTime() / 1000
+    ) {
+      return next(new AppError('Token expired', 407));
+    }
+    req.userLoged = tokenData;
+    next();
+  }catch(e){
+    if(e.name === 'TokenExpiredError') return next(new AppError('Token expired', 407));
+    return next(new AppError('Undefined token error', 500));  
   }
-  const token = autorization.split(' ')[1];
-  const tokenData = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  if (!tokenData.email) {
-    return next(new AppError('Invalids token', 401));
-  }
-  const user = await userModel.getUser(tokenData.email);
-  if (user.rows <= 0) {
-    return next(new AppError('User does not exist', 401));
-  }
-  if (
-    user.rows[0].changedPasswordAt &&
-    tokenData.iat < user.rows[0].changedPasswordAt.getTime() / 1000
-  ) {
-    return next(new AppError('Token expired', 407));
-  }
-  req.userLoged = tokenData;
-  next();
 });
