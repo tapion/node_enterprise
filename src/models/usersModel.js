74455b425d9a9d005efb8e1f2a7e55c96539a0ef
Miguel Vargas <puntadelanza86@gmail.com>
2021-01-10 16:39:20 -0500

    Se ajusta nombre de columna

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -10,7 +10,7 @@ exports.createUser = async (user, password) => {
 
 exports.getUser = async (email) => {
   return await db.query(
-    `SELECT email, "userName", name, password, "changedPasswordAt" FROM users where email = $1 
+    `SELECT email, "userName", "firstName" || ' ' || "lastName" as name, password, "changedPasswordAt" FROM users where email = $1 
       and state = true and deleted = false`,
     [email]
   );
